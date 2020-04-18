0875c41e2cd538315b4f25559ccda97bf80cd7da
Thais de Campos <thaisd.campos@gmail.com>
2020-04-17 21:10:36 -0300

    Basic manager offer CRUD

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,53 @@
+class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(permitted_params)
+
+    if @offer.save!
+      redirect_to offer_path(@offer)
+    end
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+    @offer.update(permitted_params)
+
+    if @offer.save!
+      redirect_to offer_path(@offer)
+    end
+  end
+
+  def destroy
+    @offer = Offer.find(params[:id]).destroy
+
+    redirect_to offers_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:offer).permit(
+      :advertiser_name,
+      :url,
+      :description,
+      :active_from,
+      :active_until,
+      :premium
+    )
+  end
+end
