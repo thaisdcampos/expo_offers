04f6b0c8b9ad99e86a22e233081427c6b3cc43a1
Thais de Campos <thais.campos@youse.com.br>
2020-04-19 17:49:13 -0300

    Create homepage (#3)
    * Fix seeds
    
    * add a homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-Offer.create!(advertiser_name: 'Ofeta de comida', url: 'www.big.com.br', description: 'Super desconto no big', active_from: (Date.current + 1.day), premium: true )
-Offer.create!(advertiser_name: 'Ofeta de roupa', url: 'www.dafiti.com.br', description: 'Super desconto na dafiti', active_from: (Date.current + 4.days), premium: true )
-Offer.create!(advertiser_name: 'Ofeta de esportes', url: 'www.centauro.com.br', description: 'Desconto na centauro', active_from: (Date.current + 3.days), premium: false )
-Offer.create!(advertiser_name: 'Ofeta de calçados', url: 'www.netshoes.com.br', description: 'Desconto em calçados', active_from: (Date.current + 2.days, premium: false )
-Offer.create!(advertiser_name: 'Ofeta de brinquedos', url: 'www.hihappy.com.br', description: 'Desconto em brinquedos', active_from: (Date.current + 1.day), premium: false )
+Offer.create!(advertiser_name: 'Oferta de comida', url: 'www.big.com.br', description: 'Super desconto no big', active_from: (Date.current + 1.day), premium: true, status: :enabled )
+Offer.create!(advertiser_name: 'Oferta de roupa', url: 'www.dafiti.com.br', description: 'Super desconto na dafiti', active_from: (Date.current + 4.days), premium: true, status: :disabled)
+Offer.create!(advertiser_name: 'Oferta de esportes', url: 'www.centauro.com.br', description: 'Desconto na centauro', active_from: (Date.current + 3.days), premium: false, status: :disabled )
+Offer.create!(advertiser_name: 'Oferta de calçados', url: 'www.netshoes.com.br', description: 'Desconto em calçados', active_from: (Date.current + 2.days), premium: false, status: :enabled )
+Offer.create!(advertiser_name: 'Oferta de brinquedos', url: 'www.hihappy.com.br', description: 'Desconto em brinquedos', active_from: (Date.current + 1.day), premium: false, status: :enabled )
 
